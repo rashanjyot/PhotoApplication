a8869d1ea72d79c56040e86b623453e4da3a3204
Rashanjyot <rashanjyot@yahoo.com>
2020-09-15 05:46:48 +0530

    added permission requester

--- app/src/main/java/com/rashan/photoapplication/ui/detail/activity/DetailActivity.kt
+++ app/src/main/java/com/rashan/photoapplication/ui/detail/activity/DetailActivity.kt
@@ -1,10 +1,14 @@
 package com.rashan.photoapplication.ui.detail.activity
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import com.rashan.photoapplication.R
 import com.rashan.photoapplication.base.BaseActivity
 import com.rashan.photoapplication.base.onPhotoDoubleTap
@@ -47,14 +51,67 @@ class DetailActivity : BaseActivity() {
     }
 
     private fun downloadPhoto() {
-        DownloadUtility.downloadImage(
-            applicationContext,
-            viewModel.photoLiveData.value!!.downloadUrl
-        )
+        if (ContextCompat.checkSelfPermission(
+                applicationContext,
+                EXTERNAL_STORAGE_PERMISSION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            DownloadUtility.downloadImage(
+                applicationContext,
+                viewModel.photoLiveData.value!!.downloadUrl
+            )
+        } else {
+            initiatePermissionRequest()
+        }
+    }
+
+    private fun initiatePermissionRequest() {
+        when {
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                EXTERNAL_STORAGE_PERMISSION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                downloadPhoto()
+            }
+
+            shouldShowRequestPermissionRationale(EXTERNAL_STORAGE_PERMISSION) -> {
+                Toast.makeText(
+                    applicationContext,
+                    "Please provide storage permission",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            else -> {
+                requestPermissions(arrayOf(EXTERNAL_STORAGE_PERMISSION), 100)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    downloadPhoto()
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        "Please provide storage permission",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return
+            }
+        }
     }
 
     companion object {
 
+        private const val EXTERNAL_STORAGE_PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE
+        private const val EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 1234
         const val EXTRA_PHOTO_ID = "EXTRA_PHOTO_ID"
 
         fun startActivity(activity: Activity, photoId: String) {
